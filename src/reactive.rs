3fd15017d55d7981766187ca78dc4357fbdc075d
zahash <zahash.z@gmail.com>
2023-06-27 10:54:30 +0530

    change signature of derived.

--- src/reactive.rs
+++ src/reactive.rs
@@ -19,13 +19,13 @@ impl<T> Reactive<T> {
         }
     }
 
-    pub fn derived<U: Default + Clone + PartialEq + 'static>(
-        reactive: &Reactive<T>,
+    pub fn derive<U: Default + Clone + PartialEq + 'static>(
+        &self,
         f: impl Fn(&T) -> U + 'static,
     ) -> Reactive<U> {
         let derived: Reactive<U> = Default::default();
 
-        reactive.add_observer({
+        self.add_observer({
             let derived = derived.clone();
             move |value| derived.update(|_| f(value))
         });
@@ -115,77 +115,6 @@ impl<T: Hash> ReactiveInner<T> {
     }
 }
 
-// class Reactive<T> {
-//     private _val: T;
-//     private observers: ((value: T) => void)[] = [];
-
-//     constructor(initVal: T) {
-//       this._val = initVal;
-//     }
-
-//     val(): T {
-//       return this._val;
-//     }
-
-//     update(fn: (value: T) => T): void {
-//       let newValue = fn(this._val);
-//       if (this._val !== newValue) {
-//         this._val = newValue;
-//         this.observers.forEach((obs) => obs(newValue));
-//       }
-//     }
-
-//     addObserver(fn: (value: T) => void): void {
-//       this.observers.push(fn);
-//     }
-//   }
-
-//   class Derived<T, U> {
-//     private reactive: Reactive<U>;
-
-//     constructor(reactive: Reactive<T>, fn: (val: T) => U) {
-//       this.reactive = new Reactive(fn(reactive.val()));
-//       reactive.addObserver((val) => this.reactive.update((_) => fn(val)));
-//     }
-
-//     val(): U {
-//       return this.reactive.val();
-//     }
-
-//     addObserver(fn: (value: U) => void): void {
-//       this.reactive.addObserver(fn);
-//     }
-//   }
-
-//   const example1 = () => {
-//     let nums = new Reactive([1, 2, 3]);
-//     let total = new Derived(nums, (nums) => nums.reduce((acc, n) => acc + n, 0));
-
-//     console.log(total.val());
-
-//     nums.addObserver((val) => console.log(val));
-
-//     nums.update((val) => [...val, 10, 11, 12]);
-//     nums.update((val) => [...val, 20, 30, 40]);
-
-//     console.log(total.val());
-//   };
-
-//   const example2 = () => {
-//     let a = new Reactive([]);
-//     let b = new Reactive(0);
-//     let c = new Reactive(0);
-
-//     a.addObserver((nums) => b.update((_) => nums.reduce((acc, n) => acc + n, 0)));
-//     b.addObserver((val) => c.update((_) => val * 3));
-
-//     a.update((_) => [1, 2, 3]);
-
-//     console.log(a.val());
-//     console.log(b.val());
-//     console.log(c.val());
-//   };
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -221,8 +150,8 @@ mod tests {
     #[test]
     fn test_derived() {
         let a: Reactive<Vec<i32>> = Default::default();
-        let sum_of_a = Reactive::derived(&a, |nums| nums.iter().sum());
-        let three_times_sum_of_a = Reactive::derived(&sum_of_a, |val| val * 3);
+        let sum_of_a = a.derive(|nums| nums.iter().sum());
+        let three_times_sum_of_a = sum_of_a.derive(|val| val * 3);
 
         a.update(|_| vec![1, 2, 3]);
 
