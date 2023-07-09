5d5f01140d6364803d52cdb9f2ad514e4d8d9f0e
zahash <zahash.z@gmail.com>
2023-07-09 10:06:09 +0530

    add read_yaml utility function

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,24 @@
 use serde::Deserialize;
 use std::{fs::File, io::BufReader, path::Path};
 
-pub fn read_json<P, T>(path: P) -> T
+pub fn read_json<P, T>(filepath: P) -> T
 where
     P: AsRef<Path>,
     T: for<'de> Deserialize<'de>,
 {
-    let file = File::open(path).expect("File not found");
+    let file = File::open(filepath).expect("File not found");
     let reader = BufReader::new(file);
     serde_json::from_reader::<BufReader<File>, T>(reader)
         .expect("structure of the json input does not match the structure expected by `T`")
+}
+
+pub fn read_yaml<P, T>(filepath: P) -> T
+where
+    P: AsRef<Path>,
+    T: for<'de> Deserialize<'de>,
+{
+    let file = File::open(filepath).expect("File not found");
+    let reader = BufReader::new(file);
+    serde_yaml::from_reader::<BufReader<File>, T>(reader)
+        .expect("structure of the yaml input does not match the structure expected by `T`")
 }
