c629a7cf74f6045a54ad6db739b1db7ec71bebfe
zahash <zahash.z@gmail.com>
2023-08-22 17:51:44 +0530

    update game of life

--- src/game_of_life/game_of_life_stack.rs
+++ src/game_of_life/game_of_life_stack.rs
@@ -1,21 +1,20 @@
-use rayon::prelude::{IndexedParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
+use rayon::prelude::*;
 
-pub struct Game<const N: usize> {
-    grid: [[bool; N]; N],
+pub struct Game<const R: usize, const C: usize> {
+    grid: [[bool; C]; R],
 }
 
-impl<const N: usize> Game<N> {
-    pub const fn new(start: [[bool; N]; N]) -> Self {
+impl<const R: usize, const C: usize> Game<R, C> {
+    pub const fn new(start: [[bool; C]; R]) -> Self {
         Self { grid: start }
     }
 
-    pub const fn grid(&self) -> &[[bool; N]; N] {
+    pub const fn grid(&self) -> &[[bool; C]; R] {
         &self.grid
     }
 
-    /// same as `update` but parallelized for large grids
-    pub fn par_update(&mut self) {
-        let mut next = [[false; N]; N];
+    pub fn update(&mut self) {
+        let mut next = [[false; C]; R];
 
         next.par_iter_mut().enumerate().for_each(|(r, row)| {
             row.par_iter_mut().enumerate().for_each(|(c, cell)| {
@@ -23,23 +22,11 @@ impl<const N: usize> Game<N> {
             });
         });
 
-        self.grid = next;
-    }
-
-    pub fn update(&mut self) {
-        let mut next = [[false; N]; N];
-
-        for r in 0..N {
-            for c in 0..N {
-                next[r][c] = self.next(r, c);
-            }
-        }
-
-        self.grid = next;
+        std::mem::swap(&mut self.grid, &mut next);
     }
 
     #[inline]
-    fn next(&self, r: usize, c: usize) -> bool {
+    const fn next(&self, r: usize, c: usize) -> bool {
         match (self.grid[r][c], self.live_neighbors(r, c)) {
             (true, n) if n < 2 => false,
             (true, n) if n == 2 || n == 3 => true,
@@ -51,24 +38,100 @@ impl<const N: usize> Game<N> {
 
     #[inline]
     const fn live_neighbors(&self, r: usize, c: usize) -> u8 {
-        self.grid[Self::wrap(r, -1)][Self::wrap(c, -1)] as u8
-            + self.grid[Self::wrap(r, -1)][c] as u8
-            + self.grid[Self::wrap(r, -1)][Self::wrap(c, 1)] as u8
-            + self.grid[r][Self::wrap(c, -1)] as u8
-            + self.grid[r][Self::wrap(c, 1)] as u8
-            + self.grid[Self::wrap(r, 1)][Self::wrap(c, -1)] as u8
-            + self.grid[Self::wrap(r, 1)][c] as u8
-            + self.grid[Self::wrap(r, 1)][Self::wrap(c, 1)] as u8
+        self.grid[Self::diff::<R>(r, -1)][Self::diff::<C>(c, -1)] as u8
+            + self.grid[Self::diff::<R>(r, -1)][c] as u8
+            + self.grid[Self::diff::<R>(r, -1)][Self::diff::<C>(c, 1)] as u8
+            + self.grid[r][Self::diff::<C>(c, -1)] as u8
+            + self.grid[r][Self::diff::<C>(c, 1)] as u8
+            + self.grid[Self::diff::<R>(r, 1)][Self::diff::<C>(c, -1)] as u8
+            + self.grid[Self::diff::<R>(r, 1)][c] as u8
+            + self.grid[Self::diff::<R>(r, 1)][Self::diff::<C>(c, 1)] as u8
     }
 
     #[inline]
-    const fn wrap(i: usize, diff: isize) -> usize {
+    const fn diff<const I: usize>(i: usize, diff: isize) -> usize {
         match (i, diff) {
-            (0, -1) => N - 1,
-            (_, 1) if i == N - 1 => 0,
+            (0, -1) => I - 1,
+            (_, 1) if i == I - 1 => 0,
             (_, -1) => i - 1,
             (_, 1) => i + 1,
             _ => unreachable!(),
         }
+    }
+}
+
+pub fn term_draw<const R: usize, const C: usize>(game: &Game<R, C>) {
+    for row in game.grid() {
+        for val in row {
+            match val {
+                true => print!("{}", '⬜'),
+                false => print!("{}", '⬛'),
+            }
+        }
+        println!("");
+    }
+}
+
+fn convert<const R: usize, const C: usize>(live: char, dead: char, text: &str) -> [[bool; C]; R] {
+    let mut grid = [[false; C]; R];
+
+    for (r, line) in text.trim().lines().enumerate() {
+        for (c, char) in line.trim().chars().enumerate() {
+            if char == live {
+                grid[r][c] = true;
+            }
+            if char == dead {
+                grid[r][c] = false
+            }
+        }
+    }
+
+    grid
+}
+
+pub fn run() {
+    // let grid = convert::<10, 10>(
+    //     '⬜',
+    //     '⬛',
+    //     "
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬜⬛⬛⬛⬛
+    //     ⬛⬛⬛⬜⬛⬜⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬜⬜⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    //     ",
+    // );
+
+    let grid = convert::<13, 16>(
+        '⬜',
+        '⬛',
+        "
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬜⬛⬛⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬜⬛⬛⬛⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬜⬜⬜⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬜⬛⬛⬜⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬜⬛⬛⬛⬜⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+    ",
+    );
+
+    let mut game = Game::new(grid);
+    loop {
+        term_draw(&game);
+        std::thread::sleep(std::time::Duration::from_millis(70));
+        print!("\x1B[2J\x1B[1;1H"); // clear screen
+        game.update();
     }
 }
--- src/game_of_life/mod.rs
+++ src/game_of_life/mod.rs
@@ -3,4 +3,4 @@ pub mod game_of_life_stack;
 pub mod game_of_life_vec;
 pub mod game_of_life_vec_1d;
 
-pub use game_of_life_vec::*;
+pub use game_of_life_stack::*;
