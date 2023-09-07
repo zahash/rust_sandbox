035f3971dd5c00d7a0af94599a25b4356c7c4e10
zahash <zahash.z@gmail.com>
2023-09-08 00:44:18 +0530

    working parser. need to add assignment and exponents

--- lang/src/parse.rs
+++ lang/src/parse.rs
@@ -1,8 +1,28 @@
 use crate::lex::*;
 
 #[derive(Debug, PartialEq)]
+pub enum AdditiveExpr<'ident> {
+    Add(Box<AdditiveExpr<'ident>>, MultiplicativeExpr<'ident>),
+    Sub(Box<AdditiveExpr<'ident>>, MultiplicativeExpr<'ident>),
+    MultiplicativeExpr(MultiplicativeExpr<'ident>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum MultiplicativeExpr<'ident> {
+    Mul(Box<MultiplicativeExpr<'ident>>, Primary<'ident>),
+    Div(Box<MultiplicativeExpr<'ident>>, Primary<'ident>),
+    Primary(Primary<'ident>),
+}
+
+// #[derive(Debug, PartialEq)]
+// pub enum ExponentialExpr<'ident> {
+//     Pow(Primary<'ident>, Box<ExponentialExpr<'ident>>),
+//     Primary(Primary<'ident>),
+// }
+
+#[derive(Debug, PartialEq)]
 pub enum Primary<'ident> {
-    Parens(Box<Primary<'ident>>),
+    Parens(Box<AdditiveExpr<'ident>>),
     Num(isize),
     Ident(&'ident str),
 }
@@ -13,6 +33,54 @@ pub enum ParseError<'ident> {
     UnexpectedToken((usize, Token<'ident>)),
     MismatchedParentheses,
     SyntaxError(usize),
+}
+
+fn parse_additive<'ident, 'tokens>(
+    tokens: &'tokens [Token<'ident>],
+    pos: usize,
+) -> Option<(AdditiveExpr<'ident>, usize)> {
+    let (lhs, mut pos) = parse_multiplicative(tokens, pos)?;
+    let mut lhs = AdditiveExpr::MultiplicativeExpr(lhs);
+    while let Some(token) = tokens.get(pos) {
+        match token {
+            &Token::Plus => {
+                let (rhs, next_pos) = parse_multiplicative(tokens, pos + 1)?;
+                pos = next_pos;
+                lhs = AdditiveExpr::Add(Box::new(lhs), rhs);
+            }
+            &Token::Hyphen => {
+                let (rhs, next_pos) = parse_multiplicative(tokens, pos + 1)?;
+                pos = next_pos;
+                lhs = AdditiveExpr::Sub(Box::new(lhs), rhs);
+            }
+            _ => break,
+        }
+    }
+    Some((lhs, pos))
+}
+
+fn parse_multiplicative<'ident, 'tokens>(
+    tokens: &'tokens [Token<'ident>],
+    pos: usize,
+) -> Option<(MultiplicativeExpr<'ident>, usize)> {
+    let (lhs, mut pos) = parse_primary(tokens, pos)?;
+    let mut lhs = MultiplicativeExpr::Primary(lhs);
+    while let Some(token) = tokens.get(pos) {
+        match token {
+            &Token::Asterisk => {
+                let (rhs, next_pos) = parse_primary(tokens, pos + 1)?;
+                pos = next_pos;
+                lhs = MultiplicativeExpr::Mul(Box::new(lhs), rhs);
+            }
+            &Token::Slash => {
+                let (rhs, next_pos) = parse_primary(tokens, pos + 1)?;
+                pos = next_pos;
+                lhs = MultiplicativeExpr::Div(Box::new(lhs), rhs);
+            }
+            _ => break,
+        }
+    }
+    Some((lhs, pos))
 }
 
 fn parse_primary<'ident, 'tokens>(
@@ -29,7 +97,7 @@ fn parse_primary_parens<'ident, 'tokens>(
     pos: usize,
 ) -> Option<(Primary<'ident>, usize)> {
     if let Some(&Token::LParen) = tokens.get(pos) {
-        let (primary, pos) = parse_primary(tokens, pos + 1)?;
+        let (primary, pos) = parse_additive(tokens, pos + 1)?;
         if tokens.get(pos)? == &Token::RParen {
             return Some((Primary::Parens(Box::new(primary)), pos + 1));
         }
@@ -54,6 +122,36 @@ fn parse_primary_num<'ident, 'tokens>(
     match tokens.get(pos) {
         Some(&Token::Num(n)) => Some((Primary::Num(n), pos + 1)),
         _ => None,
+    }
+}
+
+impl<'ident> std::fmt::Display for AdditiveExpr<'ident> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AdditiveExpr::Add(lhs, rhs) => write!(f, "({}+{})", lhs, rhs),
+            AdditiveExpr::Sub(lhs, rhs) => write!(f, "({}-{})", lhs, rhs),
+            AdditiveExpr::MultiplicativeExpr(factor) => write!(f, "{}", factor),
+        }
+    }
+}
+
+impl<'ident> std::fmt::Display for MultiplicativeExpr<'ident> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MultiplicativeExpr::Mul(lhs, rhs) => write!(f, "({}*{})", lhs, rhs),
+            MultiplicativeExpr::Div(lhs, rhs) => write!(f, "({}/{})", lhs, rhs),
+            MultiplicativeExpr::Primary(factor) => write!(f, "{}", factor),
+        }
+    }
+}
+
+impl<'ident> std::fmt::Display for Primary<'ident> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Primary::Parens(expr) => write!(f, "({})", expr),
+            Primary::Num(num) => write!(f, "{}", num),
+            Primary::Ident(ident) => write!(f, "{}", ident),
+        }
     }
 }
 
@@ -84,5 +182,23 @@ mod tests {
 
         println!("{:?}", tokens);
         println!("{:?}", parse_primary(&tokens.0, 0));
+    }
+
+    #[test]
+    fn test_parse_multiplicative() {
+        let tokens = lex("a * b / c").unwrap();
+
+        println!("{:?}", tokens);
+        let expr = parse_multiplicative(&tokens.0, 0).unwrap();
+        println!("{} {}", expr.0, expr.1);
+    }
+
+    #[test]
+    fn test_parse_additive() {
+        let tokens = lex("a - b - c").unwrap();
+
+        println!("{:?}", tokens);
+        let expr = parse_additive(&tokens.0, 0).unwrap();
+        println!("{} {}", expr.0, expr.1);
     }
 }
