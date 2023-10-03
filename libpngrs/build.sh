22cc87210fd83e02adf13a4406d2f8e19b27e21e
zahash <zahash.z@gmail.com>
2023-10-03 15:19:31 +0530

    minor changes

--- libpngrs/build.sh
+++ libpngrs/build.sh
@@ -1,3 +1,6 @@
+set -e
+
+rm -rf build
 cmake -S lpng1640 -B build
 cd build
 make
--- libpngrs/test.sh
+++ libpngrs/test.sh
@@ -4,9 +4,10 @@ export srcdir="/home/zahash/code/sandboxrs/libpngrs/lpng1640"
 
 cd build
 
-# sh $srcdir/tests/pngimage-full
+echo "*********   START   *********"
+
 sh $srcdir/tests/pngimage-quick
-sh $srcdir/tests/pngstest
+
 sh $srcdir/tests/pngstest-1.8
 sh $srcdir/tests/pngstest-1.8-alpha
 sh $srcdir/tests/pngstest-linear
@@ -15,7 +16,7 @@ sh $srcdir/tests/pngstest-none
 sh $srcdir/tests/pngstest-none-alpha
 sh $srcdir/tests/pngstest-sRGB
 sh $srcdir/tests/pngstest-sRGB-alpha
-# sh $srcdir/tests/pngtest-all
+
 sh $srcdir/tests/pngunknown-IDAT
 sh $srcdir/tests/pngunknown-discard
 sh $srcdir/tests/pngunknown-if-safe
@@ -23,6 +24,13 @@ sh $srcdir/tests/pngunknown-sAPI
 sh $srcdir/tests/pngunknown-sTER
 sh $srcdir/tests/pngunknown-save
 sh $srcdir/tests/pngunknown-vpAg
+
+sh $srcdir/tests/pngvalid-progressive-interlace-standard
+sh $srcdir/tests/pngvalid-progressive-size
+sh $srcdir/tests/pngvalid-progressive-standard
+sh $srcdir/tests/pngvalid-standard
+sh $srcdir/tests/pngvalid-transform
+
 sh $srcdir/tests/pngvalid-gamma-16-to-8
 sh $srcdir/tests/pngvalid-gamma-alpha-mode
 sh $srcdir/tests/pngvalid-gamma-background
@@ -32,8 +40,9 @@ sh $srcdir/tests/pngvalid-gamma-expand16-transform
 sh $srcdir/tests/pngvalid-gamma-sbit
 sh $srcdir/tests/pngvalid-gamma-threshold
 sh $srcdir/tests/pngvalid-gamma-transform
-sh $srcdir/tests/pngvalid-progressive-interlace-standard
-sh $srcdir/tests/pngvalid-progressive-size
-sh $srcdir/tests/pngvalid-progressive-standard
-sh $srcdir/tests/pngvalid-standard
-sh $srcdir/tests/pngvalid-transform
+
+# sh $srcdir/tests/pngimage-full # too slow
+# sh $srcdir/tests/pngtest-all # expected to crash
+
+
+echo "*********   DONE   *********"
