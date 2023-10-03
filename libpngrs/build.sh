cb8930a6bca2a9cbf997c29b2c3948e5fa1603c2
zahash <zahash.z@gmail.com>
2023-10-03 15:56:36 +0530

    successfully install libpng

--- libpngrs/build.sh
+++ libpngrs/build.sh
@@ -1,6 +1,16 @@
 set -e
 
 rm -rf build
-cmake -S lpng1640 -B build
+rm -rf ./lib
+rm -rf ./include
+
+cmake -S lpng1640 -B build -DCMAKE_INSTALL_PREFIX="./"
+
 cd build
 make
+make install
+cd ..
+
+rm -rf build
+rm -rf ./bin
+rm -rf ./share
